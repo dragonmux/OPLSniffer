d739d34c5d076d47fb032f14cb02e62eda04e4fe
dx-mon <dx-mon@users.sourceforge.net>
2021-01-29 22:54:44 +0000

    Implemented latch recovery from the OPL2 load signal and piped the result into the FIFO

--- gateware/sniffer/opl2Sniffer/sniffer.py
+++ gateware/sniffer/opl2Sniffer/sniffer.py
@@ -12,11 +12,15 @@ class Sniffer(Elaboratable):
 
 		self.validIncrement = Signal()
 		self.data = Signal(16)
+		self.load = Signal()
+		self.latch = Signal()
 
 	def elaborate(self, platform):
 		m = Module()
 		opl = self.opl
 		data = self.data
+		load = self.load
+		latch = self.latch
 
 		sampleFIFO = AsyncFIFO(width = 16, depth = 1024, r_domain = 'sync', w_domain = 'opl')
 		m.submodules.sampleFIFO = sampleFIFO
@@ -24,10 +28,13 @@ class Sniffer(Elaboratable):
 
 		m.d.comb += [
 			ClockSignal('opl').eq(opl.oplClk),
-			sampleFIFO.w_data.eq(data)
+			sampleFIFO.w_data.eq(data),
+			sampleFIFO.w_en.eq(latch)
 		]
 
 		m.d.opl += [
-			data.eq(Cat(data[1:], opl.data))
+			data.eq(Cat(data[1:], opl.data)),
+			load.eq(opl.load),
+			latch.eq((opl.load == 0) & (load == 1))
 		]
 		return m
