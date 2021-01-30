88766ffb1f8f7757a3d73672613d8719f64e58ae
dx-mon <dx-mon@users.sourceforge.net>
2021-01-30 02:04:32 +0000

    Hooked up a couple of signals to the GPIOs to stop the design being completely optimised out

--- gateware/sniffer/opl2Sniffer/__init__.py
+++ gateware/sniffer/opl2Sniffer/__init__.py
@@ -10,12 +10,18 @@ class OPL2Sniffer(Elaboratable):
 		from .sniffer import Sniffer
 		m = Module()
 		opl = self.opl
+		gpioA0 = platform.request('gpioA', 0)
+		gpioA1 = platform.request('gpioA', 1)
 
 		sniffer = Sniffer()
 		m.submodules.sniffer = sniffer
 		m.d.comb += [
 			sniffer.opl.oplClk.eq(opl.oplClk.i),
 			sniffer.opl.data.eq(opl.data.i),
-			sniffer.opl.load.eq(opl.load.i)
+			sniffer.opl.load.eq(opl.load.i),
+			gpioA0.oe.eq(1),
+			gpioA0.o.eq(sniffer.fifoFull),
+			gpioA1.oe.eq(1),
+			gpioA1.o.eq(sniffer.fifoReadable)
 		]
 		return m
