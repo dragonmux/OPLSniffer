b49707d06db12640f59cd2990986523f62f9c914
dx-mon <dx-mon@users.sourceforge.net>
2021-01-30 02:02:58 +0000

    Defined GPIO resources for the platform

--- gateware/sniffer/resources.py
+++ gateware/sniffer/resources.py
@@ -1,6 +1,6 @@
 from nmigen.build import Resource, Subsignal, Pins, Clock
 
-__all__ = ['OPLResource']
+__all__ = ['OPLResource', 'GPIOResources']
 
 def OPLResource(*args, load, data, oplClk, conn = None, attrs = None):
 	io = []
@@ -10,3 +10,19 @@ def OPLResource(*args, load, data, oplClk, conn = None, attrs = None):
 	if attrs is not None:
 		io.append(attrs)
 	return Resource.family(*args, default_name = 'opl', ios = io)
+
+def GPIOResources(*args, pins, default_name = 'gpio', conn = None, attrs = None):
+	assert isinstance(pins, (str, list, dict))
+
+	if isinstance(pins, str):
+		pins = pins.split()
+	if isinstance(pins, list):
+		pins = dict(enumerate(pins))
+
+	resources = []
+	for number, pin in pins.items():
+		ios = [Pins(pin, dir = 'io', conn = conn)]
+		if attrs is not None:
+			ios.append(attrs)
+		resources.append(Resource.family(*args, number, default_name = default_name, ios = ios))
+	return resources
