6fadebb766b445531cad625214d7f5995e8eb9a9
dx-mon <dx-mon@users.sourceforge.net>
2021-01-30 22:57:33 +0000

    Plugged the PIC16 softcore into the design

--- gateware/sniffer/oplSniffer.py
+++ gateware/sniffer/oplSniffer.py
@@ -5,6 +5,8 @@ __all__ = ["OPLSniffer"]
 class OPLSniffer(Elaboratable):
 	def elaborate(self, platform):
 		from .opl2Sniffer import OPL2Sniffer
+		from .pic16 import PIC16
 		m = Module()
 		m.submodules.opl2Sniffer = OPL2Sniffer(platform.request('opl'))
+		m.submodules.processor = PIC16()
 		return m
