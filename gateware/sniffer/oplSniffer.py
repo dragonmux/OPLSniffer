599e64380693bd525d0229530aea2f42e4450182
dx-mon <dx-mon@users.sourceforge.net>
2021-01-31 00:05:44 +0000

    Bonded the ROM to the processor to prevent the tools from optimising the processor out

--- gateware/sniffer/oplSniffer.py
+++ gateware/sniffer/oplSniffer.py
@@ -6,10 +6,19 @@ class OPLSniffer(Elaboratable):
 	def elaborate(self, platform):
 		from .opl2Sniffer import OPL2Sniffer
 		from .pic16 import PIC16
+		from .rom import ROM
 		m = Module()
 		m.submodules.opl2Sniffer = OPL2Sniffer(platform.request('opl'))
 		processor = PIC16()
 		m.submodules.processor = processor
+		rom = ROM()
+		m.submodules.rom = rom
+
+		m.d.comb += [
+			rom.address.eq(processor.iAddr),
+			processor.iData.eq(rom.data),
+			rom.read.eq(processor.iRead)
+		]
 
 		gpioB = platform.request('gpioB')
 		m.d.comb += [
