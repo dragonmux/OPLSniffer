2f51da2580d25ab9cc9f1cd95a2511a24c95e7fb
dx-mon <dx-mon@users.sourceforge.net>
2021-02-27 02:07:57 +0000

    Fleshed out the call stack sim some more

--- gateware/sim/sniffer/pic16/callStack.py
+++ gateware/sim/sniffer/pic16/callStack.py
@@ -39,6 +39,28 @@ def benchSync():
 	yield
 	assert (yield dut.count) == 1
 	yield
+	yield dut.valueIn.eq(0x975)
+	yield dut.push.eq(1)
+	assert (yield dut.count) == 1
+	yield
+	yield dut.push.eq(0)
+	assert (yield dut.count) == 1
+	yield
+	yield dut.pop.eq(1)
+	assert (yield dut.count) == 2
+	yield
+	yield dut.pop.eq(0)
+	assert (yield dut.count) == 2
+	assert (yield dut.valueOut) == 0x975
+	yield
+	yield dut.pop.eq(1)
+	assert (yield dut.count) == 1
+	yield
+	yield dut.pop.eq(0)
+	assert (yield dut.count) == 1
+	assert (yield dut.valueOut) == 0x713
+	yield
+	assert (yield dut.count) == 0
 	yield
 
 sim = Simulator(dut)
