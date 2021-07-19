8afc49addd82d1af1b012831532100d0ba875f71
dx-mon <dx-mon@users.sourceforge.net>
2021-07-19 04:00:45 +0100

    Removed the now extraneous definition of ROM for the Bitsy test target

--- gateware/bitsy.py
+++ gateware/bitsy.py
@@ -10,33 +10,6 @@ if (gatewarePath.parent / 'sniffer').is_dir():
 from nmigen import Elaboratable, Module, Signal, Memory, Cat, Repl, Instance
 from nmigen_boards.icebreaker_bitsy import ICEBreakerBitsyPlatform
 
-class ROM(Elaboratable):
-	def __init__(self):
-		self.data = Signal(16)
-		self.address = Signal(12)
-		self.read = Signal()
-
-		self.contents = Memory(width = 16, depth = 2 ** 12)
-
-	def elaborate(self, platform):
-		m = Module()
-		memory = self.contents
-		writePort = memory.write_port()
-		readPort = memory.read_port(transparent = False)
-		m.submodules += writePort
-		m.submodules.rom = readPort
-
-		m.d.comb += [
-			writePort.addr.eq(0),
-			writePort.data.eq(0),
-			writePort.en.eq(0),
-
-			readPort.addr.eq(self.address),
-			self.data.eq(readPort.data),
-			readPort.en.eq(self.read)
-		]
-		return m
-
 class RAM(Elaboratable):
 	def __init__(self):
 		self.readData = Signal(8)
@@ -220,6 +193,7 @@ class IOWO(Elaboratable):
 
 	def elaborate(self, platform):
 		from sniffer.pic16 import PIC16
+		from sniffer.rom import ROM
 		m = Module()
 		m.submodules.processor = processor = PIC16()
 		m.submodules.rom = rom = ROM()
