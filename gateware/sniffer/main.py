59bda0155569d7e22535ebab174b55e95095bd9b
dx-mon <dx-mon@users.sourceforge.net>
2021-07-22 19:46:40 +0100

    Begun folding execution of the sims into the CLI

--- gateware/sniffer/main.py
+++ gateware/sniffer/main.py
@@ -2,14 +2,30 @@ from .platform import OPLSnifferPlatform
 from .oplSniffer import OPLSniffer
 
 def cli():
-	from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+	from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, FileType
 	from nmigen import cli as nmigenCLI
 
 	parser = ArgumentParser(formatter_class = ArgumentDefaultsHelpFormatter,
 		description = 'OPLSniffer')
-	nmigenCLI.main_parser(parser)
-	actions = parser._subparsers._group_actions[0]
-	buildAction = actions.add_parser('build', help = 'build a bitstream from the design')
+	actions = parser.add_subparsers(dest = 'action', required = True)
+	genAction = actions.add_parser(
+		'generate', help = 'generate RTLIL, Verilog or CXXRTL from the design'
+	)
+	genAction.add_argument(
+		'-t', '--type', dest = 'generate_type', metavar = 'LANGUAGE', choices = ['il', 'cc', 'v'],
+		help = 'generate LANGUAGE (il for RTLIL, v for Verilog, cc for CXXRTL; '+
+			'default: file extension of FILE, if given)'
+	)
+	genAction.add_argument(
+		'generate_file', metavar = 'FILE', type = FileType('w'), nargs = '?',
+		help = 'write generated code to FILE'
+	)
+	actions.add_parser('build', help = 'build a bitstream from the design')
+	simAction = actions.add_parser('simulate', help = 'run simulations on the design')
+	simAction.add_argument(
+		'unit', choices = ['alu'],
+		help = 'which available unit simulation to run'
+	)
 
 	args = parser.parse_args()
 
@@ -17,6 +33,11 @@ def cli():
 	oplSniffer = OPLSniffer()
 	if args.action == 'build':
 		platform.build(oplSniffer, name = 'OPLSniffer')
-	else:
+	elif args.action == 'generate':
 		nmigenCLI.main_runner(parser, args, oplSniffer, platform = platform, name = 'OPLSniffer')
-			#, ports = oplSniffer.get_ports())
+	elif args.action == 'simulate':
+		if args.unit == 'alu':
+			from sim.sniffer.pic16.alu import sim
+		with sim.write_vcd(f'sim/{args.unit}.vcd'):
+			sim.reset()
+			sim.run()
